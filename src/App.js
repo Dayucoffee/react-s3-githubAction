4622e179654fc45f42bc8976c78bff16d434eefd
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2023-10-26 10:28:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <code>static website in s3 with Github Action; It worked!! </code> 
+        <code>static website in s3 with Github Action; testing! testing! worked!! </code> 
         </p>
         <a
           className="App-link"
